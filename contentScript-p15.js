1a89a7d702652ffa28826ff21582cd4d4103a1f6
Samuel Joseph Karetla <ksamueljoseph006@gmail.com>
2024-02-18 21:19:47 +0530

    Created content scripts for the different keyboard shortcuts

---
+++ contentScript-p15.js
@@ -0,0 +1,29 @@
+function exe_ks_p15(){
+    try{
+        const settings = document.getElementsByClassName('ytp-settings-button');
+        if(settings){
+            settings[0].click();
+        }
+
+        try{
+            const playback_speed = document.getElementsByClassName('ytp-menuitem');
+            
+            if(playback_speed){
+                playback_speed[3].click();
+                setTimeout(function(){
+                    playback_speed[5].click();
+                },1000);
+                setTimeout(function(){
+                    settings[0].click();
+                },1000);
+                
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+exe_ks_p15();
---
+++ contentScript.js
@@ -0,0 +1,7 @@
+(function() {
+    const skip_ad = document.getElementsByClassName('ytp-ad-skip-button-modern ytp-button');
+    if (skip_ad) {
+      skip_ad[0].click();
+    }
+})();
+  
